92032756c59b4272f128c173c17918d49aa09e16
giulia <giulia.cavalca@live.com>
2016-05-13 22:22:01 +0200

    top_three

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -7,6 +7,17 @@ class Bar < ApplicationRecord
       leaderboard[bet.user.id] += 1 if bet.check_winner
     end
 
-    leaderboard.to_json
+    leaderboard
   end
+
+  def top_three
+    top_three={}
+    sorted = leaderboard.sort{|l, r| l[1]<=>r[1]}
+    top_three[:first]=sorted.first[1]
+    top_three[:second]=sorted.second[1]
+    top_three[:third]=sorted.third[1]
+
+    top_three
+  end
+
 end
